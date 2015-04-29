cbf9cc087a9766b6d06b34edab9b935646019501
Avirup Nandi <avirup.nandi@tcs.com>
2015-04-29 17:45:40 +0300

    More refactoring...

--- rest-application/src/main/java/guice/sank/rest/app/filters/JerseyRestApplication.java
+++ rest-application/src/main/java/guice/sank/rest/app/filters/JerseyRestApplication.java
@@ -1,16 +1,26 @@
 package guice.sank.rest.app.filters;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import guice.sank.rest.app.apis.CmsRestApi;
+import guice.sank.rest.app.apis.RootApi;
+
+import java.util.Set;
 
 public class JerseyRestApplication extends PackagesResourceConfig {
 	public JerseyRestApplication() {
 		super(getServletPackages());
+		Set<Class<?>> classes = super.getClasses();
+		addRestModules(classes);
+	}
+
+	private void addRestModules(Set<Class<?>> classes) {
+		classes.add(RootApi.class);
+		classes.add(CmsRestApi.class);
 	}
 
 	private static String[] getServletPackages() {
 		return new String[] {
-				"com.wordnik.swagger.jaxrs.listing",
-				"guice.sank.rest.app.apis"
+				"com.wordnik.swagger.jaxrs.listing"
 		};
 	}
 }
--- rest-application/src/main/java/guice/sank/rest/app/modules/RestModule.java
+++ rest-application/src/main/java/guice/sank/rest/app/modules/RestModule.java
@@ -1,6 +1,7 @@
 package guice.sank.rest.app.modules;
 
-import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 import guice.sank.rest.app.filters.CharacterEncodingFilter;
@@ -10,19 +11,19 @@ import guice.sank.rest.app.filters.SwaggerDocumentationFilter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RestModule extends ServletModule {
+public class RestModule extends JerseyServletModule {
 
 	@Override
 	public void configureServlets() {
-		super.configureServlets();
-		filter("/api-docs", "/api-docs/*").through(SwaggerDocumentationFilter.class);
 		filter("/*").through(CharacterEncodingFilter.class);
+		filter("/api-docs", "/api-docs/*").through(SwaggerDocumentationFilter.class);
 		serve("/*").with(GuiceContainer.class, getApplicationMap());
 	}
 
 	private Map<String, String> getApplicationMap() {
 		return new HashMap<String, String>(){{
 			put(ServletContainer.APPLICATION_CONFIG_CLASS, getRestApplicationName());
+			put(ResourceConfig.FEATURE_DISABLE_WADL, "true");
 		}};
 	}
 
