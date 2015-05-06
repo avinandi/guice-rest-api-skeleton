bfd545b394d92d2a522579867cd03704d887de42
Avirup Nandi <avirup.nandi@tcs.com>
2015-05-06 09:53:03 +0300

    Added Health check api in Jersey module

--- rest-application/src/main/java/guice/sank/rest/app/filters/JerseyRestApplication.java
+++ rest-application/src/main/java/guice/sank/rest/app/filters/JerseyRestApplication.java
@@ -2,6 +2,7 @@ package guice.sank.rest.app.filters;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import guice.sank.rest.app.apis.CmsRestApi;
+import guice.sank.rest.app.apis.HealthCheckApi;
 import guice.sank.rest.app.apis.RootApi;
 import guice.sank.rest.app.exceptionMapper.RuntimeExceptionMapper;
 import guice.sank.rest.app.objectMapper.GuiceRestApplicationObjectMapperProvider;
@@ -19,6 +20,7 @@ public class JerseyRestApplication extends PackagesResourceConfig {
 
 	private void addRestModules(Set<Class<?>> classes) {
 		classes.add(RootApi.class);
+		classes.add(HealthCheckApi.class);
 		classes.add(CmsRestApi.class);
 	}
 
